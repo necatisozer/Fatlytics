c8502ce746428be3652d54985b5cc1449ceda220
necati <necatisozer@gmail.com>
2019-05-15 04:25:32 +0300

    Add daily goal chart

--- app/src/main/java/com/fatlytics/app/data/source/api/FatlyticsManager.kt
+++ app/src/main/java/com/fatlytics/app/data/source/api/FatlyticsManager.kt
@@ -1,137 +1,168 @@
 package com.fatlytics.app.data.source.api
 
 import com.fatlytics.app.data.source.api.entity.FoodEntriesGet
+import com.fatlytics.app.domain.entity.DailyActiveness
+import com.fatlytics.app.domain.entity.Gender
+import com.fatlytics.app.domain.entity.User
+import com.fatlytics.app.domain.repository.UserRepository
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import org.threeten.bp.LocalDate
+import org.threeten.bp.Period
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class FatlyticsManager @Inject constructor(
-    private val fatlyticsApi: FatlyticsApi
+    private val fatlyticsApi: FatlyticsApi,
+    private val userRepository: UserRepository
 ) {
     fun getFood(id: Long, amount: Double): Single<Food> {
-        return fatlyticsApi.getFood(id).map {
-            Food(
-                id = it.food?.food_id?.toLongOrNull(),
-                name = it.food?.food_name,
-                servings = it.food?.servings?.serving?.map {
-                    Serving(
-                        id = it.serving_id?.toLongOrNull(),
-                        numberOfUnits = it.number_of_units?.toDoubleOrNull()?.times(amount),
-                        measurementDescription = it.measurement_description,
-                        contents = listOf(
-                            ServingContent(
-                                name = "Calories",
-                                amount = it.calories?.toDoubleOrNull()?.times(amount),
-                                measure = "cal"
-                            ),
-                            ServingContent(
-                                name = "Total Fat",
-                                amount = it.fat?.toDoubleOrNull()?.times(amount),
-                                measure = "g"
-                            ),
-                            ServingContent(
-                                name = "Saturated Fat",
-                                amount = it.saturated_fat?.toDoubleOrNull()?.times(amount),
-                                measure = "g",
-                                subContent = true
-                            ),
-                            ServingContent(
-                                name = "Polyunsaturated Fat",
-                                amount = it.polyunsaturated_fat?.toDoubleOrNull()?.times(amount),
-                                measure = "g",
-                                subContent = true
-                            ),
-                            ServingContent(
-                                name = "Monounsaturated Fat",
-                                amount = it.monounsaturated_fat?.toDoubleOrNull()?.times(amount),
-                                measure = "g",
-                                subContent = true
-                            ),
-                            ServingContent(
-                                name = "Cholesterol",
-                                amount = it.cholesterol?.toDoubleOrNull()?.times(amount),
-                                measure = "mg"
-                            ),
-                            ServingContent(
-                                name = "Sodium",
-                                amount = it.sodium?.toDoubleOrNull()?.times(amount),
-                                measure = "mg"
-                            ),
-                            ServingContent(
-                                name = "Potassium",
-                                amount = it.potassium?.toDoubleOrNull()?.times(amount),
-                                measure = "mg"
-                            ),
-                            ServingContent(
-                                name = "Total Carbohydrate",
-                                amount = it.carbohydrate?.toDoubleOrNull()?.times(amount),
-                                measure = "g"
-                            ),
-                            ServingContent(
-                                name = "Dietary Fiber",
-                                amount = it.fiber?.toDoubleOrNull()?.times(amount),
-                                measure = "g",
-                                subContent = true
-                            ),
-                            ServingContent(
-                                name = "Sugars",
-                                amount = it.sugar?.toDoubleOrNull()?.times(amount),
-                                measure = "g",
-                                subContent = true
-                            ),
-                            ServingContent(
-                                name = "Protein",
-                                amount = it.protein?.toDoubleOrNull()?.times(amount),
-                                measure = "g"
-                            ),
-                            ServingContent(
-                                name = "Vitamin A",
-                                amount = it.vitamin_a?.toDoubleOrNull(),
-                                measure = "%"
-                            ),
-                            ServingContent(
-                                name = "Vitamin C",
-                                amount = it.vitamin_c?.toDoubleOrNull(),
-                                measure = "%"
-                            ),
-                            ServingContent(
-                                name = "Calcium",
-                                amount = it.calcium?.toDoubleOrNull(),
-                                measure = "%"
-                            ),
-                            ServingContent(
-                                name = "Iron",
-                                amount = it.iron?.toDoubleOrNull(),
-                                measure = "%"
+
+        return getFoodEntries().flatMap { foodEntries ->
+            fatlyticsApi.getFood(id).map {
+                Food(
+                    id = it.food?.food_id?.toLongOrNull(),
+                    name = it.food?.food_name,
+                    servings = it.food?.servings?.serving?.map {
+                        Serving(
+                            id = it.serving_id?.toLongOrNull(),
+                            numberOfUnits = it.number_of_units?.toDoubleOrNull()?.times(amount),
+                            measurementDescription = it.measurement_description,
+                            contents = listOf(
+                                ServingContent(
+                                    name = "Calories",
+                                    amount = it.calories?.toDoubleOrNull()?.times(amount),
+                                    measure = "cal"
+                                ),
+                                ServingContent(
+                                    name = "Total Fat",
+                                    amount = it.fat?.toDoubleOrNull()?.times(amount),
+                                    measure = "g"
+                                ),
+                                ServingContent(
+                                    name = "Saturated Fat",
+                                    amount = it.saturated_fat?.toDoubleOrNull()?.times(amount),
+                                    measure = "g",
+                                    subContent = true
+                                ),
+                                ServingContent(
+                                    name = "Polyunsaturated Fat",
+                                    amount = it.polyunsaturated_fat?.toDoubleOrNull()?.times(amount),
+                                    measure = "g",
+                                    subContent = true
+                                ),
+                                ServingContent(
+                                    name = "Monounsaturated Fat",
+                                    amount = it.monounsaturated_fat?.toDoubleOrNull()?.times(amount),
+                                    measure = "g",
+                                    subContent = true
+                                ),
+                                ServingContent(
+                                    name = "Cholesterol",
+                                    amount = it.cholesterol?.toDoubleOrNull()?.times(amount),
+                                    measure = "mg"
+                                ),
+                                ServingContent(
+                                    name = "Sodium",
+                                    amount = it.sodium?.toDoubleOrNull()?.times(amount),
+                                    measure = "mg"
+                                ),
+                                ServingContent(
+                                    name = "Potassium",
+                                    amount = it.potassium?.toDoubleOrNull()?.times(amount),
+                                    measure = "mg"
+                                ),
+                                ServingContent(
+                                    name = "Total Carbohydrate",
+                                    amount = it.carbohydrate?.toDoubleOrNull()?.times(amount),
+                                    measure = "g"
+                                ),
+                                ServingContent(
+                                    name = "Dietary Fiber",
+                                    amount = it.fiber?.toDoubleOrNull()?.times(amount),
+                                    measure = "g",
+                                    subContent = true
+                                ),
+                                ServingContent(
+                                    name = "Sugars",
+                                    amount = it.sugar?.toDoubleOrNull()?.times(amount),
+                                    measure = "g",
+                                    subContent = true
+                                ),
+                                ServingContent(
+                                    name = "Protein",
+                                    amount = it.protein?.toDoubleOrNull()?.times(amount),
+                                    measure = "g"
+                                ),
+                                ServingContent(
+                                    name = "Vitamin A",
+                                    amount = it.vitamin_a?.toDoubleOrNull(),
+                                    measure = "%"
+                                ),
+                                ServingContent(
+                                    name = "Vitamin C",
+                                    amount = it.vitamin_c?.toDoubleOrNull(),
+                                    measure = "%"
+                                ),
+                                ServingContent(
+                                    name = "Calcium",
+                                    amount = it.calcium?.toDoubleOrNull(),
+                                    measure = "%"
+                                ),
+                                ServingContent(
+                                    name = "Iron",
+                                    amount = it.iron?.toDoubleOrNull(),
+                                    measure = "%"
+                                )
                             )
                         )
-                    )
-                }
-            )
+                    }
+                )
+            }
         }
     }
 
     fun getFoodEntries(): Single<FoodEntriesModel> {
-        return fatlyticsApi.getFoodEntries().map {
-            it.food_entries?.food_entry?.let {
-                val foodEntries = it.groupBy { it.meal }
-                FoodEntriesModel(
-                    breakfast = foodEntries["Breakfast"].let { it } ?: listOf(),
-                    breakfastTotalCal = foodEntries["Breakfast"]?.let { it.sumBy { it.calories.toIntOrZero() } }
-                        ?: 0,
-                    lunch = foodEntries["Lunch"].let { it } ?: listOf(),
-                    lunchTotalCal = foodEntries["Lunch"]?.let { it.sumBy { it.calories.toIntOrZero() } }
-                        ?: 0,
-                    dinner = foodEntries["Dinner"].let { it } ?: listOf(),
-                    dinnerTotalCal = foodEntries["Dinner"]?.let { it.sumBy { it.calories.toIntOrZero() } }
-                        ?: 0,
-                    other = foodEntries["Other"].let { it } ?: listOf(),
-                    otherTotalCal = foodEntries["Other"]?.let { it.sumBy { it.calories.toIntOrZero() } }
-                        ?: 0
-                )
-            } ?: FoodEntriesModel()
+        return userRepository.getCurrentUser().firstOrError().flatMap { user ->
+            fatlyticsApi.getFoodEntries().map {
+                it.food_entries?.food_entry?.let {
+                    val foodEntries = it.groupBy { it.meal }
+                    FoodEntriesModel(
+                        breakfast = foodEntries["Breakfast"].let { it } ?: listOf(),
+                        breakfastTotalCal = foodEntries["Breakfast"]?.let { it.sumBy { it.calories.toIntOrZero() } }
+                            ?: 0,
+                        lunch = foodEntries["Lunch"].let { it } ?: listOf(),
+                        lunchTotalCal = foodEntries["Lunch"]?.let { it.sumBy { it.calories.toIntOrZero() } }
+                            ?: 0,
+                        dinner = foodEntries["Dinner"].let { it } ?: listOf(),
+                        dinnerTotalCal = foodEntries["Dinner"]?.let { it.sumBy { it.calories.toIntOrZero() } }
+                            ?: 0,
+                        other = foodEntries["Other"].let { it } ?: listOf(),
+                        otherTotalCal = foodEntries["Other"]?.let { it.sumBy { it.calories.toIntOrZero() } }
+                            ?: 0,
+                        dailyGoal = dailyGoal(user)
+                    )
+                } ?: FoodEntriesModel()
+            }.subscribeOn(Schedulers.io())
         }
+    }
+
+    private fun dailyGoal(user: User): Int {
+        val age = Period.between(user.personalInfo?.birthday, LocalDate.now()).years
+        var calorie = 10 * user.healthInfo?.weight!! + 6.25 * user.healthInfo.height!!
+
+        when (user.personalInfo?.gender) {
+            Gender.MALE -> calorie = calorie - 5 * age + 5
+            Gender.FEMALE -> calorie = calorie + 5 * age - 161
+        }
+
+        when (user.healthInfo.dailyActiveness) {
+            DailyActiveness.NOT_ACTIVE -> calorie *= 1.2
+            DailyActiveness.LOW -> calorie *= 1.375
+            DailyActiveness.MEDIUM -> calorie *= 1.55
+            DailyActiveness.HIGH -> calorie *= 1.725
+        }
+
+        return calorie.toInt()
     }
 }
 
@@ -165,9 +196,10 @@ data class FoodEntriesModel(
     val dinner: List<FoodEntriesGet.FoodEntries.FoodEntry> = listOf(),
     val dinnerTotalCal: Int = 0,
     val other: List<FoodEntriesGet.FoodEntries.FoodEntry> = listOf(),
-    val otherTotalCal: Int = 0
+    val otherTotalCal: Int = 0,
+    val dailyGoal: Int = 0
 ) {
-    fun getTotalCal() = breakfastTotalCal + lunchTotalCal + dinnerTotalCal + otherTotalCal
+    fun totalCal() = breakfastTotalCal + lunchTotalCal + dinnerTotalCal + otherTotalCal
 }
 
 private fun String?.toIntOrZero() = this?.toIntOrNull()?.let { it } ?: 0
--- app/src/main/java/com/fatlytics/app/ui/main/diary/DiaryFragment.kt
+++ app/src/main/java/com/fatlytics/app/ui/main/diary/DiaryFragment.kt
@@ -1,15 +1,28 @@
 package com.fatlytics.app.ui.main.diary
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.RelativeSizeSpan
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fatlytics.app.R
+import com.fatlytics.app.data.source.api.FoodEntriesModel
 import com.fatlytics.app.databinding.DiaryFragmentBinding
 import com.fatlytics.app.extension.onSingleClick
 import com.fatlytics.app.ui.addfood.AddFoodActivity
 import com.fatlytics.app.ui.base.BaseViewModelFragment
+import com.fatlytics.app.ui.main.home.CalorieFormatter
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import splitties.fragments.start
+import java.util.ArrayList
 
 class DiaryFragment : BaseViewModelFragment<DiaryViewModel, DiaryFragmentBinding>() {
     override val layoutRes = R.layout.diary_fragment
@@ -27,6 +40,8 @@ class DiaryFragment : BaseViewModelFragment<DiaryViewModel, DiaryFragmentBinding
     }
 
     private fun initView() {
+        initChart()
+
         binding.breakfastRecycler.apply {
             adapter = breakfastFoodEntryAdapter
             addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
@@ -72,15 +87,120 @@ class DiaryFragment : BaseViewModelFragment<DiaryViewModel, DiaryFragmentBinding
         }
     }
 
+    private fun initChart() {
+        val chart = binding.halfPieChart
+        chart!!.setBackgroundColor(Color.WHITE)
+
+        //chart!!.setUsePercentValues(true)
+        chart!!.description.isEnabled = false
+
+        chart!!.setCenterTextTypeface(
+            Typeface.createFromAsset(
+                context?.assets,
+                "OpenSans-Light.ttf"
+            )
+        )
+
+        chart!!.isDrawHoleEnabled = true
+        chart!!.setHoleColor(Color.WHITE)
+
+        chart!!.setTransparentCircleColor(Color.WHITE)
+        chart!!.setTransparentCircleAlpha(110)
+
+        chart!!.holeRadius = 58f
+        chart!!.transparentCircleRadius = 61f
+
+        chart!!.setDrawCenterText(true)
+
+        chart!!.isRotationEnabled = false
+        chart!!.isHighlightPerTapEnabled = true
+
+        chart!!.maxAngle = 180f // HALF CHART
+        chart!!.rotationAngle = 180f
+        chart!!.setCenterTextOffset(0f, -20f)
+
+        chart!!.animateY(1400, Easing.EaseInOutQuad)
+
+        val l = chart!!.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        l.orientation = Legend.LegendOrientation.HORIZONTAL
+        l.setDrawInside(false)
+        l.xEntrySpace = 7f
+        l.yEntrySpace = 0f
+        l.yOffset = 0f
+
+        // entry label styling
+        chart!!.setEntryLabelColor(Color.BLACK)
+        chart!!.setEntryLabelTypeface(
+            Typeface.createFromAsset(
+                context?.assets,
+                "OpenSans-Regular.ttf"
+            )
+        )
+        chart!!.setEntryLabelTextSize(12f)
+    }
+
+    private fun generateCenterSpannableText(text: String): SpannableString {
+        val s = SpannableString(text)
+        s.setSpan(RelativeSizeSpan(1.7f), 0, s.length, 0)
+        return s
+    }
+
+    private fun setData(foodEntriesModel: FoodEntriesModel) {
+        val chart = binding.halfPieChart
+
+        chart!!.centerText =
+            generateCenterSpannableText("Daily Goal:\n${foodEntriesModel.dailyGoal} cal")
+
+        val values = ArrayList<PieEntry>()
+
+        values.add(PieEntry(foodEntriesModel.totalCal().toFloat(), "Food"))
+        values.add(
+            PieEntry(
+                (foodEntriesModel.dailyGoal - foodEntriesModel.totalCal()).toFloat(),
+                "Remaining"
+            )
+        )
+
+        val dataSet = PieDataSet(values, "")
+        dataSet.sliceSpace = 3f
+        dataSet.selectionShift = 5f
+
+        dataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
+        //dataSet.setSelectionShift(0f);
+
+        val data = PieData(dataSet)
+        data.setValueFormatter(CalorieFormatter())
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.BLACK)
+        data.setValueTypeface(
+            Typeface.createFromAsset(
+                context?.assets,
+                "OpenSans-Light.ttf"
+            )
+        )
+        chart!!.data = data
+
+        chart!!.invalidate()
+    }
+
     private fun observeViewModel() {
         viewModel.init()
         viewModel.breakfastFoodEntries.observe(viewLifecycleOwner, Observer {
+            setData(it)
             breakfastFoodEntryAdapter.submitList(it.breakfast)
             lunchFoodEntryAdapter.submitList(it.lunch)
             dinnerFoodEntryAdapter.submitList(it.dinner)
             otherFoodEntryAdapter.submitList(it.other)
             binding.breakfastTotalCalories.text =
                 getString(R.string.cal, it.breakfastTotalCal.toString())
+            binding.lunchTotalCalories.text =
+                getString(R.string.cal, it.lunchTotalCal.toString())
+            binding.dinnerTotalCalories.text =
+                getString(R.string.cal, it.dinnerTotalCal.toString())
+            binding.otherTotalCalories.text =
+                getString(R.string.cal, it.otherTotalCal.toString())
         })
     }
 }
